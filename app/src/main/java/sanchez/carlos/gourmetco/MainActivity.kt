787f15d86813c41d422e9c06710a232ab794d0b8
cristicn2024 <cristi_cn_2004@outlook.com>
2025-03-06 00:52:05 -0700

    eliminacion de scroll en fragmentos

--- app/src/main/java/sanchez/carlos/gourmetco/MainActivity.kt
+++ app/src/main/java/sanchez/carlos/gourmetco/MainActivity.kt
@@ -1,6 +1,8 @@
 package sanchez.carlos.gourmetco
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ScrollView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -20,18 +22,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_trending,R.id.nav_bookmark, R.id.nav_create, R.id.nav_profile
+                R.id.nav_home, R.id.nav_trending, R.id.nav_bookmark, R.id.nav_create, R.id.nav_profile
             )
         )
 
-        // setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // Escuchar cambios en los fragmentos
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.nav_home) {
+                binding.scrollView2.visibility = View.VISIBLE  // Mostrar en Home
+            } else {
+                binding.scrollView2.visibility = View.GONE  // Ocultar en otros fragmentos
+            }
+        }
     }
 }
